cf1c4e740d33fed2cf4544075ba1f49dfe935da0
juan <120036984@upq.edu.mx>
2023-07-31 17:34:12 -0600

    terminado

--- balance.php
+++ balance.php
@@ -54,7 +54,17 @@ include 'funciones.php';
 $conexion = conectarBD();
 
 //encontramos el nombre Costo de ventas, Ventas y Gastos de operacion de la tabla ${cuenta}_balanza
-$sql = "SELECT * FROM ${cuenta}_balanza WHERE NombreEsquema = 'Costo de ventas' OR NombreEsquema = 'Ventas' OR NombreEsquema = 'Gastos de operacion' AND ${cuenta}_ID = $ID";
+$sql = "SELECT * FROM ${cuenta}_balanza WHERE NombreEsquema = 'Costo de ventas' 
+OR NombreEsquema = 'Ventas' 
+OR NombreEsquema = 'Gastos de operacion' 
+OR NombreEsquema = 'Bancos'
+OR NombreEsquema = 'Clientes'
+OR NombreEsquema = 'Almacen'
+OR NombreEsquema = 'Proveedores'
+OR NombreEsquema = 'Equipo de Oficina'
+OR NombreEsquema = 'Capital Social'
+OR NombreEsquema = 'Acreedores'
+AND ${cuenta}_ID = $ID";
 
 //ejecucion de la consulta
 $resultado = $conexion->query($sql);
@@ -66,6 +76,20 @@ $suma_costo_de_ventas_deudor = 0;
 $suma_costo_de_ventas_acreedor = 0;
 $suma_gastos_de_operacion_deudor = 0;
 $suma_gastos_de_operacion_acreedor = 0;
+$suma_Bancos_deudor = 0;
+$suma_Bancos_acreedor = 0;
+$suma_Clientes_deudor = 0;
+$suma_Clientes_acreedor = 0;
+$suma_Almacen_deudor = 0;
+$suma_Almacen_acreedor = 0;
+$suma_Proveedores_deudor = 0;
+$suma_Proveedores_acreedor = 0;
+$suma_Equipo_de_Oficina_deudor = 0;
+$suma_Equipo_de_Oficina_acreedor = 0;
+$suma_Capital_Social_deudor = 0;
+$suma_Capital_Social_acreedor = 0;
+$suma_Acreedores_deudor = 0;
+$suma_Acreedores_acreedor = 0;
 
 //si el resultado es mayor a 0
 if ($resultado->num_rows > 0) {
@@ -81,23 +105,44 @@ if ($resultado->num_rows > 0) {
         } elseif ($fila['NombreEsquema'] === 'Gastos de operación') {
             $suma_gastos_de_operacion_deudor += $fila['Total_saldo_deudor'];
             $suma_gastos_de_operacion_acreedor += $fila['Total_saldo_acreedor'];
+        } elseif ($fila['NombreEsquema'] === 'Bancos') {
+            $suma_Bancos_deudor += $fila['Total_saldo_deudor'];
+            $suma_Bancos_acreedor += $fila['Total_saldo_acreedor'];
+        } elseif ($fila['NombreEsquema'] === 'Clientes') {
+            $suma_Clientes_deudor += $fila['Total_saldo_deudor'];
+            $suma_Clientes_acreedor += $fila['Total_saldo_acreedor'];
+        } elseif ($fila['NombreEsquema'] === 'Almacen') {
+            $suma_Almacen_deudor += $fila['Total_saldo_deudor'];
+            $suma_Almacen_acreedor += $fila['Total_saldo_acreedor'];
+        } elseif ($fila['NombreEsquema'] === 'Proveedores') {
+            $suma_Proveedores_deudor += $fila['Total_saldo_deudor'];
+            $suma_Proveedores_acreedor += $fila['Total_saldo_acreedor'];
+        } elseif ($fila['NombreEsquema'] === 'Equipo de Oficina') {
+            $suma_Equipo_de_Oficina_deudor += $fila['Total_saldo_deudor'];
+            $suma_Equipo_de_Oficina_acreedor += $fila['Total_saldo_acreedor'];
+        } elseif ($fila['NombreEsquema'] === 'Capital Social') {
+            $suma_Capital_Social_deudor += $fila['Total_saldo_deudor'];
+            $suma_Capital_Social_acreedor += $fila['Total_saldo_acreedor'];
+        } elseif ($fila['NombreEsquema'] === 'Acreedores') {
+          $suma_Acreedores_deudor += $fila['Total_saldo_deudor'];
+          $suma_Acreedores_acreedor += $fila['Total_saldo_acreedor'];
         }
-    }
+      }
 }
 
 //cerrar la conexion
 $conexion->close();
 
-//imprimir la tabla con los totales
+//imprimir la tabla estado de resultados
 echo "<div class='container'>";
 echo "<div class='table-container'>";
 echo "<table class='table table-hover table-dark'>";
 echo "<tr>";
-echo "<th colspan='2' style='text-align: center;'>Balanza de saldos</th>";
+echo "<th colspan='2' style='text-align: center;'>Estado de resultados</th>";
 echo "</tr>";
 echo "<tr>";
-echo "<th style='text-align: center;'>Nombre de las cuentas</th>";
-echo "<th style='text-align: center;'>Total saldo deudor</th>";
+echo "<th style='text-align: center;'>Nombres</th>";
+echo "<th style='text-align: center;'>Cantidad</th>";
 echo "</tr>";
 
 // Filas para Ventas
@@ -130,6 +175,184 @@ $total_general = $total_deudor - $suma_gastos_de_operacion_deudor;
 echo "<tr>";
 echo "<td style='text-align: center;'></td>";
 echo "<td style='text-align: center;'><strong>$total_general</strong></td>";
+echo "</tr>";
+
+echo "</table>";
+echo "</div>";
+echo "</div>";
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+//imprimir la tabla Activo circulante
+echo "<div class='container'>";
+echo "<div class='table-container'>";
+echo "<table class='table table-hover table-dark'>";
+echo "<tr>";
+echo "<th colspan='2' style='text-align: center;'><strong>Activo Circulante<strong></th>";
+echo "</tr>";
+echo "<tr>";
+echo "<th style='text-align: center;'>Nombres</th>";
+echo "<th style='text-align: center;'>Cantidad</th>";
+echo "</tr>";
+
+// Filas para Bancos
+echo "<tr>";
+echo "<td style='text-align: center;'>Bancos</td>";
+echo "<td style='text-align: center;'>$suma_Bancos_deudor</td>";
+echo "</tr>";
+
+// Filas para Costo de Clientes
+echo "<tr>";
+echo "<td style='text-align: center;'>Clientes</td>";
+echo "<td style='text-align: center;'>$suma_Clientes_deudor</td>";
+echo "</tr>";
+
+// Filas para Gastos de Almacen
+echo "<tr>";
+echo "<td style='text-align: center;'>Almacen</td>";
+echo "<td style='text-align: center;'>$suma_Almacen_deudor</td>";
+echo "</tr>";
+
+$total_general_AC = $suma_Bancos_deudor + $suma_Clientes_deudor + $suma_Almacen_deudor;
+// Filas para Total Activo Circulante
+echo "<tr>";
+echo "<td style='text-align: center;'>Total Activo Circulante</td>";
+echo "<td style='text-align: center;'><strong>$total_general_AC</strong></td>";
+echo "</tr>";
+
+echo "</table>";
+echo "</div>";
+echo "</div>";
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+//imprimir la Fijo
+echo "<div class='container'>";
+echo "<div class='table-container'>";
+echo "<table class='table table-hover table-dark'>";
+echo "<tr>";
+echo "<th colspan='2' style='text-align: center;'><strong>Fijo<strong></th>";
+echo "</tr>";
+echo "<tr>";
+echo "<th style='text-align: center;'>Nombres</th>";
+echo "<th style='text-align: center;'>Cantidad</th>";
+echo "</tr>";
+
+// Filas para Equipos de oficina
+echo "<tr>";
+echo "<td style='text-align: center;'>Equipos de oficina</td>";
+echo "<td style='text-align: center;'>$suma_Equipo_de_Oficina_deudor</td>";
+echo "</tr>";
+
+$total_general_F = $suma_Equipo_de_Oficina_deudor;
+// Filas para Total Activo Circulante
+echo "<tr>";
+echo "<td style='text-align: center;'>Total Activo Circulante</td>";
+echo "<td style='text-align: center;'><strong>$total_general_F</strong></td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td style='text-align: center;'></td>";
+echo "<td style='text-align: center;'></td>";
+echo "</tr>";
+
+$total_general_A = $total_general_F + $total_general_AC;
+// Filas para Gastos de operacion
+echo "<tr>";
+echo "<td style='text-align: center;'><strong>Total de los activos</strong></td>";
+echo "<td style='text-align: center;'><strong>$total_general_A</strong></td>";
+echo "</tr>";
+
+echo "</table>";
+echo "</div>";
+echo "</div>";
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+//imprimir la tabla pasivo
+echo "<div class='container'>";
+echo "<div class='table-container'>";
+echo "<table class='table table-hover table-dark'>";
+echo "<tr>";
+echo "<th colspan='2' style='text-align: center;'><strong>Pasivo<strong></th>";
+echo "</tr>";
+echo "<tr>";
+echo "<th style='text-align: center;'>Nombres</th>";
+echo "<th style='text-align: center;'>Cantidad</th>";
+echo "</tr>";
+
+// Filas para Proovedores
+echo "<tr>";
+echo "<td style='text-align: center;'>Proovedores</td>";
+echo "<td style='text-align: center;'>$suma_Proveedores_acreedor</td>";
+echo "</tr>";
+
+// Filas para Acreedores
+echo "<tr>";
+echo "<td style='text-align: center;'>Acreedores</td>";
+echo "<td style='text-align: center;'>$suma_Acreedores_acreedor</td>";
+echo "</tr>";
+
+$total_general_P = $suma_Proveedores_acreedor + $suma_Acreedores_acreedor;
+// Filas para Total Activo Circulante
+echo "<tr>";
+echo "<td style='text-align: center;'><strong>Total Activo Circulante</strong></td>";
+echo "<td style='text-align: center;'><strong>$total_general_P</strong></td>";
+echo "</tr>";
+
+echo "</table>";
+echo "</div>";
+echo "</div>";
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+//imprimir la tabla capital
+echo "<div class='container'>";
+echo "<div class='table-container'>";
+echo "<table class='table table-hover table-dark'>";
+echo "<tr>";
+echo "<th colspan='2' style='text-align: center;'><strong>Capital<strong></th>";
+echo "</tr>";
+echo "<tr>";
+echo "<th style='text-align: center;'>Nombres</th>";
+echo "<th style='text-align: center;'>Cantidad</th>";
+echo "</tr>";
+
+// Filas para Capital Social
+echo "<tr>";
+echo "<td style='text-align: center;'>Capital Social</td>";
+echo "<td style='text-align: center;'>$suma_Capital_Social_acreedor</td>";
+echo "</tr>";
+
+// Filas para Utilidad del Ejercicio
+echo "<tr>";
+echo "<td style='text-align: center;'>Utilidad del Ejercicio</td>";
+echo "<td style='text-align: center;'>$total_general</td>";
+echo "</tr>";
+
+$total_general_C = $suma_Capital_Social_acreedor + $total_general;
+// Filas para Total Activo Circulante
+echo "<tr>";
+echo "<td style='text-align: center;'><strong>Total Activo Circulante</strong></td>";
+echo "<td style='text-align: center;'><strong>$total_general_C</strong></td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td style='text-align: center;'></td>";
+echo "<td style='text-align: center;'></td>";
+echo "</tr>";
+
+$total_general_Cap = $total_general_C + $total_general_P;
+// Filas para Gastos de operacion
+echo "<tr>";
+echo "<td style='text-align: center;'><strong>Total del capital</strong></td>";
+echo "<td style='text-align: center;'><strong>$total_general_Cap</strong></td>";
 echo "</tr>";
 
 echo "</table>";
